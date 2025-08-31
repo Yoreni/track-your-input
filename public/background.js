97238500d9ed3a51f0b696b9fa4224ed5838928d
Yoreni <yoreni@icloud.com>
2025-08-31 17:40:01 +0100

    added delete button

--- public/background.js
+++ public/background.js
@@ -1,24 +1,52 @@
-browser.runtime.onMessage.addListener((message) => {
-    if (message.type === "addWatchTime") {
-        browser.storage.local.get('youtubeWatchTimes').then((data) => 
+const HOUR_CUTOFF = 4
+
+function normaliseDay(date)
+{
+    const cutoffToday = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate(),
+        HOUR_CUTOFF,
+    );
+    if (date >= cutoffToday) 
+        return cutoffToday;
+    const cutoffYesterday = new Date(cutoffToday);
+    cutoffYesterday.setDate(cutoffYesterday.getDate() - 1);
+    return cutoffYesterday;
+}
+
+ browser.runtime.onMessage.addListener((message) => 
+{
+    if (message.type === "addWatchTime") 
+    {
+        browser.storage.local.get('youtubeWatchTimes').then(async (data) => 
         {
             const {language, id, time} = message
-
+            const day = normaliseDay(new Date()).toISOString().split("T")[0]
+            const entryId = `${id}${day}`
             let times = data.youtubeWatchTimes || {};
-            const currentTime = times[id]?.time || 0
-            const timestamp = currentTime?.date ?? new Date()
-            const newWatchData = {
-                time: Math.round(currentTime + time),
-                // language: message.language,
-                date: timestamp.toISOString(),
-                description: ""
-            }
 
             if (!times[language])
                 times[language] = {}
-            times[language][id] = newWatchData;
-            browser.storage.local.set({youtubeWatchTimes: times});
-            browser.storage.local.set({"currentId": message.id});
+
+            if (times[language][entryId])
+            {
+                let entry = times[language][entryId]
+                entry.time += Math.round(time)
+                times[language][entryId] = entry
+            }
+            else
+            {
+                const entry = 
+                {
+                    time: Math.round(time),
+                    date: new Date().toISOString(),
+                    description: ""
+                }
+                times[language][entryId] = entry
+            }
+        
+            await browser.storage.local.set({youtubeWatchTimes: times});
         });
     }
 });
--- src/popup/HistoryPage.tsx
+++ src/popup/HistoryPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import type { EditWatchDataEntry, WatchDataEntry } from "../WatchData"
 
 interface Props 
@@ -19,7 +20,7 @@ export function HistoryPage( {input, deleteInput, editInput}: Props )
     input; deleteInput; editInput; 
     return (
         <div className="p-2 bg-gray-200 dark:bg-gray-800 mx-auto text-black dark:text-white">
-            <table className="min-w-full divide-y divide-gray-700 text-sm"> {/* Smaller text size for table */}
+            <table className="min-w-full divide-y divide-gray-700 text-sm">
                 <thead>
                     <tr className="bg-gray-400 dark:bg-gray-600">
                         <th className="px-2 py-1 text-left text-xs font-medium tracking-wider">Description</th>
@@ -28,7 +29,8 @@ export function HistoryPage( {input, deleteInput, editInput}: Props )
                     </tr>
                 </thead>
                 <tbody className="bg-gray-800 divide-y divide-gray-700">
-                    {input.map((entry, index) => <Row key={entry.id} entry={entry} isEven={index % 2 === 0}/>)} 
+                    {input.map((entry, index) => <Row key={entry.id} entry={entry} isEven={index % 2 === 0} 
+                        deleteInput={() => deleteInput(entry.id)} editInput={(values: EditWatchDataEntry) => editInput(entry.id, values)}/>)} 
                 </tbody>
             </table>
         </div>
@@ -39,23 +41,26 @@ interface RowProps
 {
     entry: WatchDataEntry;
     isEven: boolean;
-    // deleteInput: (id: string) => void;
-    // editInput: (id: string, values: EditWatchDataEntry) => void;
+    deleteInput: () => void;
+    editInput: (values: EditWatchDataEntry) => void;
 }
 
-function Row( {entry, isEven}: RowProps)
+function Row( {entry, isEven, deleteInput}: RowProps)
 {
-    console.log(isEven)
+    const [editDialogOpen, setEditDialogOpen] = useState(false)
+
     const rowClasses = isEven ? "dark:bg-gray-800 bg-gray-200" : "dark:bg-gray-700 bg-gray-300";
     return (
-        <tr className={rowClasses}>
-            <td className="px-2 py-1 max-w-50 overflow-ellipsis overflow-hidden text-nowrap">{entry.description || entry.id}</td> {/* Adjusted max-width for description */}
-            <td className="px-2 py-1">{formatTime(entry.time)}</td>
-            <td className="px-2 py-1 text-center whitespace-nowrap">
-                <button className="bg-green-600 hover:bg-green-700 text-white text-xs py-0.5 px-1 rounded mr-1">✏️</button>
-                <button className="bg-red-600 hover:bg-red-700 text-white text-xs py-0.5 px-1 rounded">🗑️</button>
-            </td>
-        </tr>
+        <>
+            <tr className={rowClasses}>
+                <td className="px-2 py-1 max-w-50 overflow-ellipsis overflow-hidden text-nowrap">{entry.description || entry.id}</td> {/* Adjusted max-width for description */}
+                <td className="px-2 py-1">{formatTime(entry.time)}</td>
+                <td className="px-2 py-1 text-center whitespace-nowrap">
+                    <button className="bg-green-600 hover:bg-green-700 text-white text-xs py-0.5 px-1 rounded mr-1">✏️</button>
+                    <button className="bg-red-600 hover:bg-red-700 text-white text-xs py-0.5 px-1 rounded" onClick={deleteInput}>🗑️</button>
+                </td>
+            </tr>
+        </>
     )
 }
 
