868708c9c8d06983e7b88a2a7d682303c6e2a7cc
Yoreni <yoreni@icloud.com>
2025-09-10 20:11:13 +0100

    videos with unknown language are ignored

--- public/background.js
+++ public/background.js
@@ -22,6 +22,9 @@ function normaliseDay(date)
         browser.storage.local.get('youtubeWatchTimes').then(async (data) => 
         {
             const {language, id, time, description, inputType} = message
+            if (language === "unknown")
+                return
+
             const day = normaliseDay(new Date()).toISOString().split("T")[0]
             const entryId = `${id}${day}`
             let times = data.youtubeWatchTimes || {};
